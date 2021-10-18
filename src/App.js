5ab7ab0f0cad10f7a6bc877add40c66ccf84b3d6
Cathy Yang <cyang7@stanford.edu>
2021-10-17 22:17:42 -0700

    updated icons and added text box

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
+import React, { useState } from 'react'; 
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+  const [gentext, setGentext] = useState("");
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    alert(`Submitting Name ${text}`);
+    setGentext(text);
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {/* cat counter */}
+      <button type="button" onClick={() => setCount(count + 1)}>
+        Increase cat count: {count}
+      </button>
+      <CatCounter count={count} />
+      {/* emotjipasta */}
+      <form onSubmit={handleSubmit}>
+      <label>
+        Your text:
+        <textarea
+          width= "300px"
+          height= "150px"
+          type="text"
+          value={text}
+          onChange={e => setText(e.target.value)}
+        />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+    <p>generated text</p>
+    <p> {gentext}</p>
+    <EmojiPasta name = {gentext}/>
       </header>
     </div>
+  );
+}
+
+function EmojiPasta({text}) {
+  return (
+    <>
+      {text}
+    </>
+  );
+}
+
+function CatCounter({count}) {
+  let cats = [];
+  for (let i = 0; i < count; i++) {
+    cats.push('🐱');
+  }
+  return (
+    <>
+      {cats}
+    </>
   );
 }
 
