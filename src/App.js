d097675483bcc3ca564a02b9fa9ce6d266aa5b55
Cathy Yang <cyang7@stanford.edu>
2021-10-18 00:37:35 -0700

    added page formatting

--- src/App.js
+++ src/App.js
@@ -1,67 +1,81 @@
-import React, { useState } from 'react'; 
-import logo from './logo.svg';
-import './App.css';
-
-
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./Slide.scss";
+import { generateEmojipasta } from "./emojipasta";
+const getEmoji = require("get-random-emoji");
 
 function App() {
   const [count, setCount] = useState(0);
   const [text, setText] = useState("");
   const [gentext, setGentext] = useState("");
+  const [emojis, setEmojis] = useState([]);
+
   const handleSubmit = (evt) => {
     evt.preventDefault();
-    alert(`Submitting Name ${text}`);
-    setGentext(text);
-  }
+    let newtext = generateEmojipasta(text);
+    // alert(`new  text is ${newtext}`);
+    setGentext(newtext);
+  };
   return (
     <div className="App">
+      <script src="./emoji-mappings.js"></script>
       <header className="App-header">
+        <p>emoji</p>
         <img src={logo} className="App-logo" alt="logo" />
-      {/* cat counter */}
-      <button type="button" onClick={() => setCount(count + 1)}>
-        Increase cat count: {count}
-      </button>
-      <CatCounter count={count} />
-      {/* emotjipasta */}
-      <form onSubmit={handleSubmit}>
-      <label>
-        Your text:
-        <textarea
-          width= "300px"
-          height= "150px"
-          type="text"
-          value={text}
-          onChange={e => setText(e.target.value)}
-        />
-      </label>
-      <input type="submit" value="Submit" />
-    </form>
-    <p>generated text</p>
-    <p> {gentext}</p>
-    <EmojiPasta name = {gentext}/>
+        <p>fun</p>
       </header>
+      <body>
+        <h1>Build an emoji string</h1>
+        <div display="flex" flex-direction="row">
+          <button
+            class="icon-slide-left"
+            background-color="blue"
+            onClick={() => setEmojis(emojis.concat(getEmoji()))}
+          >
+            <span>Add emoji</span>
+          </button>
+          <button
+            class="icon-slide-right"
+            onClick={() => setEmojis(emojis.slice(0, -1))}
+          >
+            <span>Remove emoji</span>
+          </button>
+        </div>
+        <button
+          class="button-rotate-grow"
+          onClick={() => setEmojis(buildEmojis(emojis.length))}
+        >
+          Random emoji string of length {emojis.length}
+        </button>
+        <p> {emojis}</p>
+        {/* <EmojiBuilder count={count} /> */}
+
+        <h1>Build an emoji pasta</h1>
+        <form onSubmit={handleSubmit}>
+          <textarea
+            width="300px"
+            height="150px"
+            type="text"
+            value={text.toString()}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <input type="submit" value="Generate" />
+        </form>
+        <p> {gentext}</p>
+      </body>
     </div>
   );
 }
 
-function EmojiPasta({text}) {
-  return (
-    <>
-      {text}
-    </>
-  );
-}
-
-function CatCounter({count}) {
-  let cats = [];
+function buildEmojis(count) {
+  let myemojis = [];
   for (let i = 0; i < count; i++) {
-    cats.push('🐱');
+    let em = getEmoji();
+    myemojis.push(em);
   }
-  return (
-    <>
-      {cats}
-    </>
-  );
+
+  return myemojis;
 }
 
 export default App;
